b33f5e9af7176c2c21c9c620ee326ee980f2a235
SalahHW <salah.bouheniche@gmail.com>
2023-07-07 23:41:05 +0200

    add parser functions

--- include/so_long.h
+++ include/so_long.h
@@ -6,8 +6,14 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 21:03:34 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/07/06 21:03:55 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/07/07 23:33:41 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libs/libft/libft.h"
+#include <stdlib.h>
 
+//		parser.c		//
+void	parse(int argc, char **argv);
+//		arg_error.c			//
+void	exit_error_arg(void);
