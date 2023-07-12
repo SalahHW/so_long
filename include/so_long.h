de49251094fcdcbf051387789bd816c00ea00a82
SalahHW <salah.bouheniche@gmail.com>
2023-07-12 19:27:07 +0200

    move libft path

--- include/so_long.h
+++ include/so_long.h
@@ -6,14 +6,14 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 21:03:34 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/07/12 17:34:09 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/07/12 19:26:22 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../lib/libft/libft.h"
+#include "../libft/libft.h"
 #include <stdlib.h>
 
 //		parser.c		//
 void	parse(int argc, char **argv);
-//		arg_error.c			//
-void	exit_error_arg(void);
+//		exit_error.c
+void	printed_exit_error(char *msg);
