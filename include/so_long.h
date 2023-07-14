1c4e34fff2d12109f2110b73e4ebfd34b42e1df2
salah <salah.bouheniche@gmail.com>
2023-07-14 02:05:32 +0200

    add map_parser WIP

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 21:03:34 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/07/14 00:27:26 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/07/14 02:04:19 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+typedef struct s_map_elem_count
+{
+	int start;
+	int	exit;
+	int item;
+} t_map_elem_count;
+
 //		parser.c		//
 void	parse_argument(int argc, char **argv);
 void	parse_map(int fd);
+//		map_parser.c	//
 //		exit_error.c
 void	printed_exit_error(char *msg);
--- src/map_parser.c
+++ src/map_parser.c
@@ -1,23 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   map_parser.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/07 21:38:02 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/07/14 01:54:58 by sbouheni         ###   ########.fr       */
+/*   Created: 2023/07/14 01:54:03 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/07/14 02:04:25 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-int	main(int argc, char **argv)
-{
-	int	map_fd;
 
-	parse_argument(argc, argv);
-	argv ++;
-	map_fd = open(*argv, O_RDWR);
-	parse_map(map_fd);
-}
