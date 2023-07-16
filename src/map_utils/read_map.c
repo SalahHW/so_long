9bf2597fd1cc057975ece60c94f17c6b84b0e894
SalahHW <salah.bouheniche@gmail.com>
2023-07-16 22:26:47 +0200

    fix bug

--- src/map_utils/read_map.c
+++ src/map_utils/read_map.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 19:17:50 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/07/16 17:29:48 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/07/16 22:08:24 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,16 @@ t_map_edge	*read_map(int fd)
 		printed_exit_error("File is empty");
 	map = malloc(sizeof(t_map_edge));
 	attribute = malloc(sizeof(t_map_attribute));
+	if (!map || !attribute)
+		printed_exit_error("Memory allocation error");
 	init_map(map);
 	init_attribute(attribute);
 	map->attribute = attribute;
 	while (line)
 	{
-		push_front(map, line);
-		line = get_next_line(fd);
+		push_back(map, line);
 		map->attribute->height++;
+		line = get_next_line(fd);
 	}
 	return (map);
 }
