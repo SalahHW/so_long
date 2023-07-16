f05ecafa4c6ac0ebdbcd1fb0c9cbc6acb5594ef2
SalahHW <salah.bouheniche@gmail.com>
2023-07-16 22:26:21 +0200

    fix function that was pushing from front instead of back

--- src/list_utils/push.c
+++ src/list_utils/push.c
@@ -6,25 +6,25 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 12:02:41 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/07/16 17:35:42 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/07/16 21:59:53 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-void	push_front(t_map_edge *list, char *line)
+void	push_back(t_map_edge *list, char *line)
 {
-	t_map	*new_line;
+	t_map	*new_element;
 
-	new_line = malloc(sizeof(t_map));
-	if (!new_line)
+	new_element = malloc(sizeof(t_map));
+	if (!new_element)
 		printed_exit_error("Memory allocation problem");
-	new_line->line = line;
-	new_line->next = list->first;
-	new_line->prev = NULL;
-	if (list->first)
-		list->first->prev = new_line;
+	new_element->line = line;
+	new_element->prev = list->last;
+	new_element->next = NULL;
+	if (list->last)
+		list->last->next = new_element;
 	else
-		list->last = new_line;
-	list->first = new_line;
+		list->first = new_element;
+	list->last = new_element;
 }
