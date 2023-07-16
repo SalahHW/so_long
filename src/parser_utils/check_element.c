d6414694f2878d2c78091c3648b56f45cc74f07c
SalahHW <salah.bouheniche@gmail.com>
2023-07-16 18:34:12 +0200

    add function that check if map contains correct element

---
+++ src/parser_utils/check_element.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_element.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/14 21:37:45 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/07/16 17:57:06 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/so_long.h"
+
+void	check_element(t_map_edge *map)
+{
+	t_map	*map_ptr;
+	char	*line_ptr;
+
+	map_ptr = map->first;
+	while (map_ptr)
+	{
+		line_ptr = map_ptr->line;
+		while (*line_ptr && *line_ptr != '\n')
+		{
+			if (*line_ptr == '0')
+				;
+			else if (*line_ptr == '1')
+				;
+			else if (*line_ptr == 'C')
+				map->attribute->item++;
+			else if (*line_ptr == 'E')
+				map->attribute->exit++;
+			else if (*line_ptr == 'P')
+				map->attribute->player++;
+			else
+				printed_exit_error("Map contains wrong elements");
+			line_ptr++;
+		}
+		map_ptr = map_ptr->next;
+	}
+}
