ba66e5019849e385166d9d09580843d9f380cf28
SalahHW <salah.bouheniche@gmail.com>
2023-07-16 18:33:33 +0200

    fix function that read map file

--- src/map_utils/read_map.c
+++ src/map_utils/read_map.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 19:17:50 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/07/14 21:20:18 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/07/16 17:29:48 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 t_map_edge	*read_map(int fd)
 {
-	char		*line;
-	t_map_edge	*map;
+	char			*line;
+	t_map_edge		*map;
+	t_map_attribute	*attribute;
 
 	if (fd < 0)
 		printed_exit_error("File not found");
@@ -23,11 +24,15 @@ t_map_edge	*read_map(int fd)
 	if (!line)
 		printed_exit_error("File is empty");
 	map = malloc(sizeof(t_map_edge));
-	init(map);
+	attribute = malloc(sizeof(t_map_attribute));
+	init_map(map);
+	init_attribute(attribute);
+	map->attribute = attribute;
 	while (line)
 	{
 		push_front(map, line);
 		line = get_next_line(fd);
+		map->attribute->height++;
 	}
 	return (map);
 }
