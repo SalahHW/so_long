07d722101527997b4b7af97d9b1c2ad4f63d3565
SalahHW <salah.bouheniche@gmail.com>
2023-08-12 13:46:21 +0200

    fix norm

--- src/game_utils/position.c
+++ src/game_utils/position.c
@@ -6,19 +6,19 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 21:46:54 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/12 01:59:31 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/12 13:36:39 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-int		is_valid_position(t_map *map, int x, int y)
+int	is_valid_position(t_map *map, int x, int y)
 {
 	if (is_wall(map->map_grid[y][x]))
-		return 0;
+		return (0);
 	if (is_enemy(map->map_grid[y][x]))
 		exit(1);
-		//game_over();
+	//game_over();
 	if (is_item(map->map_grid[y][x]))
 		map->item--;
 	if (is_portal(map->map_grid[y][x]))
@@ -27,6 +27,5 @@ int		is_valid_position(t_map *map, int x, int y)
 			exit(1);
 		//	game_win();
 	}
-
 	return (1);
 }
