c4d4d5615d12781312e3278a393127a0a5d7e28d
SalahHW <salah.bouheniche@gmail.com>
2023-08-16 02:41:44 +0200

    add delay between frames

--- src/rendering/animation.c
+++ src/rendering/animation.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 16:02:53 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/15 20:49:41 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/16 02:30:42 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int	trigger_animation(t_map *map)
 {
+	static int	delay_count;
+	int			max_delay;
+
+	max_delay = 60;
+	mlx_clear_window(map->mlx_ptr, map->window);
 	render_window(map);
-	map->frame++;
-	if (map->frame > 8)
-		map->frame = 0;
+	if (delay_count >= max_delay)
+	{
+		map->frame++;
+		if (map->frame > 8)
+			map->frame = 1;
+		delay_count = 0;
+	}
+	delay_count++;
 	return (0);
 }
