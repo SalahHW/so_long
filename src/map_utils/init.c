ed5227af15695afb2f7c3f540270fe89442f8e7f
SalahHW <salah.bouheniche@gmail.com>
2023-08-16 02:43:18 +0200

    clean code

--- src/map_utils/init.c
+++ src/map_utils/init.c
@@ -6,26 +6,14 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 16:06:58 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/15 19:21:40 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/15 21:38:13 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-void	init_attribute(t_map *attribute)
+static void	init_pointer(t_map *attribute)
 {
-	attribute->height = 0;
-	attribute->width = 0;
-	attribute->start = 0;
-	attribute->exit = 0;
-	attribute->item = 0;
-	attribute->player_x_position = 0;
-	attribute->player_y_position = 0;
-	attribute->portal_x_position = 0;
-	attribute->portal_y_position = 0;
-	attribute->enemy = 0;
-	attribute->move_counter = 0;
-	attribute->frame = 1;
 	attribute->mlx_ptr = NULL;
 	attribute->window = NULL;
 	attribute->ground_img = NULL;
@@ -41,4 +29,21 @@ void	init_attribute(t_map *attribute)
 	attribute->portal6_img = NULL;
 	attribute->portal7_img = NULL;
 	attribute->portal8_img = NULL;
+}
+
+void	init_attribute(t_map *attribute)
+{
+	attribute->height = 0;
+	attribute->width = 0;
+	attribute->start = 0;
+	attribute->exit = 0;
+	attribute->item = 0;
+	attribute->player_x_position = 0;
+	attribute->player_y_position = 0;
+	attribute->portal_x_position = 0;
+	attribute->portal_y_position = 0;
+	attribute->enemy = 0;
+	attribute->move_counter = 0;
+	attribute->frame = 1;
+	init_pointer(attribute);
 }
