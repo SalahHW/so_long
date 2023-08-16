d7f93dd5b14f1ac6982fe0bd5603c23b8941caee
SalahHW <salah.bouheniche@gmail.com>
2023-08-16 02:55:59 +0200

    add a move counter in game window

--- src/rendering/animation.c
+++ src/rendering/animation.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 16:02:53 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/16 02:30:42 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/16 02:54:17 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	trigger_animation(t_map *map)
 {
 	static int	delay_count;
 	int			max_delay;
+	char		*move_count;
 
 	max_delay = 60;
 	mlx_clear_window(map->mlx_ptr, map->window);
@@ -27,6 +28,9 @@ int	trigger_animation(t_map *map)
 			map->frame = 1;
 		delay_count = 0;
 	}
+	move_count = ft_itoa(map->move_counter);
+	mlx_string_put(map->mlx_ptr, map->window, 10, 30, 0XFFFFFF, move_count);
+	free(move_count);
 	delay_count++;
 	return (0);
 }
